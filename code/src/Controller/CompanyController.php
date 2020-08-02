8527dc7bb47cdf03002126add2d977fcfb27a76b
Alejandro Zorita Hernández <alzort@gmail.com>
2020-08-02 16:57:08 +0200

    Controller BD

--- code/src/Controller/CompanyController.php
+++ code/src/Controller/CompanyController.php
@@ -4,16 +4,25 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Company;
 
 class CompanyController extends AbstractController
 {
     /**
      * @Route("/company", name="company")
+     * @param  \Doctrine\ORM\EntityManagerInterface  $entityManager
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function index()
+    public function index(EntityManagerInterface $entityManager)
     {
+        $companies = $entityManager
+          ->getRepository(Company::class)
+          ->findAll();
         return $this->render('company/index.html.twig', [
             'controller_name' => 'CompanyController',
+            'companies' => $companies
         ]);
     }
 }
