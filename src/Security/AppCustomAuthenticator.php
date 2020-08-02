dacbe74a51f5a19b46539b41a49c8710c2359bb1
Alejandro Zorita Hernández <alzort@gmail.com>
2020-08-02 18:58:21 +0200

    Update AppCustomAuthenticator.php

--- src/Security/AppCustomAuthenticator.php
+++ src/Security/AppCustomAuthenticator.php
@@ -96,6 +96,11 @@ class AppCustomAuthenticator extends AbstractFormLoginAuthenticator implements P
             return new RedirectResponse($targetPath);
         }
 
+        if ( in_array( 'ROLE_ADMIN', $token->getUser()->getRoles() ) ) {
+
+            return new RedirectResponse($this->urlGenerator->generate('company'));
+        }
+
         // For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
         throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
