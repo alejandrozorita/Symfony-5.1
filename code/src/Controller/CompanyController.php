964a92ceff086d3ea4c8b96e49e85786e1bd560a
Alejandro Zorita Hernández <alzort@gmail.com>
2020-08-02 18:18:21 +0200

    Persistencia de datos

--- code/src/Controller/CompanyController.php
+++ code/src/Controller/CompanyController.php
@@ -2,10 +2,12 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-use Doctrine\ORM\EntityManagerInterface;
 use App\Entity\Company;
+use App\Form\CompanyType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 class CompanyController extends AbstractController
 {
@@ -21,8 +23,39 @@ class CompanyController extends AbstractController
           ->getRepository(Company::class)
           ->findAll();
         return $this->render('company/index.html.twig', [
-            'controller_name' => 'CompanyController',
-            'companies' => $companies
+          'controller_name' => 'CompanyController',
+          'companies' => $companies
         ]);
     }
+
+
+    /**
+     * @param  Request  $request
+     * @param  EntityManagerInterface  $entityManager
+     *
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     * @Route("/company/new", name="company_new")
+     */
+    public function create(Request $request, EntityManagerInterface $entityManager)
+    {
+        $form = $this->createForm(CompanyType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+            $company = new Company();
+            $company->setName($data['name']);
+            $company->setEmail($data['email']);
+
+            $entityManager->persist($company);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('company');
+        }
+
+        return $this->render('company/new.html.twig', [
+          'form' => $form->createView(),
+        ]);
+    }
+
 }
--- code/src/Entity/Company.php
+++ code/src/Entity/Company.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use App\Repository\CompanyRepository;
+use ArrayAccess;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
@@ -12,6 +13,7 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class Company
 {
+
     /**
      * @ORM\Id()
      * @ORM\GeneratedValue()
